6f225252faf60002fd1fcbe2871d611c795b0045
cipdv <cip.devries@gmail.com>
2020-03-13 13:58:58 -0400

    added mongoose user class

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require ('express');
 const app = express();
+const mongoose = require ('mongoose');
+require ('./services/passport');
+require ('./routes/authRoutes')(app);
+const keys = require ('./config/keys');
+require ('./models/User');
 
-app.get('/', (req, res)=>{
-    res.send({hi: 'Cippy'})
-})
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
