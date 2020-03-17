06e0fd9cc1407a569144a198086c0e622c2d1667
cipdv <cip.devries@gmail.com>
2020-03-17 17:18:26 -0400

    finished auth flow

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require ('express');
-const app = express();
 const mongoose = require ('mongoose');
-require ('./services/passport');
-require ('./routes/authRoutes')(app);
+const cookieSession = require ('cookie-session');
+const passport = require ('passport');
 const keys = require ('./config/keys');
 require ('./models/User');
+require ('./services/passport');
 
 mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use (
+    cookieSession ({
+        maxAge: 120 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use (
+    passport.initialize()
+);
+
+app.use (
+    passport.session()
+);
+
+
+
+require ('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
