da4f6c9451614f02e8fc55c89f536872cb7351da
cipdv <cip.devries@gmail.com>
2020-03-23 12:50:00 -0400

    setting up auth reducers for header

---
+++ client/src/components/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+
+import Header from './Header';
+const Dashboard = ()=> <h2>dashboard</h2>;
+const SurveyNew = ()=> <h2>survey new</h2>;
+const Landing = ()=> <h2>landing</h2>;
+
+
+class App extends React.Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render () {
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route path="/" exact component={Landing} />
+                        <Route path="/surveys" component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
+};
+ 
+export default connect(null, actions)(App);
+
--- services/passport.js
+++ services/passport.js
@@ -21,15 +21,14 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-}, (accessToken, refreshToken, profile, done)=>{
-    User.findOne({googleId: profile.id}).then(existingUser => {
-        if (existingUser) {
+}, async (accessToken, refreshToken, profile, done)=>{
+    const existingUser = await User.findOne({googleId: profile.id});     
+    if (existingUser) {
             done (null, existingUser);
         } else {
-            new User ({
+            const user = await new User ({
                 googleId: profile.id
             }).save()
-            .then (user => done (null, user));
+            done (null, user);
         }
-    })  
-}));
+    }));
