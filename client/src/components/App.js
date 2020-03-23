2f10c08d5d4449db27373653009a74bfd5157d1e
cipdv <cip.devries@gmail.com>
2020-03-23 13:55:04 -0400

    troubleshooting

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
 
 import Header from './Header';
+import { Component } from 'react';
 const Dashboard = ()=> <h2>dashboard</h2>;
 const SurveyNew = ()=> <h2>survey new</h2>;
 const Landing = ()=> <h2>landing</h2>;
 
 
-class App extends React.Component {
+class App extends Component{
     componentDidMount() {
         this.props.fetchUser();
     }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,38 +1,74 @@
-import React from 'react';
-import {connect} from 'react-redux';
+// import React, {Component} from 'react';
+// import {connect} from 'react-redux';
 
 
-class Header extends React.Component {
-    renderContent () {
-        switch (this.props.auth) {
-            case null:
-                return;
-            case false:
-                return <li><a href="/auth/google">Sign in</a></li>;
-            default:
-                return <li><a>Logout</a></li>
-        }
-    }
+// class Header extends Component {
+//     renderContent () {
+//         switch (this.props.auth) {
+//             case null:
+//                 return;
+//             case false:
+//                 return <li><a href="/auth/google">Sign in</a></li>;
+//             default:
+//                 return <li>Logout</li>
+//         }
+//     }
 
-    render () {
-        return (
-            <nav>
-                <div className="nav-wrapper">
-                    <a className="left brand-logo">
-                        Emaily
-                    </a>
-                    <ul className="right">
-                        {this.renderContent()}
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
+//     render () {
+//         return (
+//             <nav>
+//                 <div className="nav-wrapper">
+//                     <a className="left brand-logo">
+//                         Emaily
+//                     </a>
+//                     <ul className="right">
+//                         {this.renderContent()}
+//                     </ul>
+//                 </div>
+//             </nav>
+//         )
+//     }
     
+// }
+
+// function mapStateToProps (auth) {
+//     return {auth}
+// };
+
+// export default connect(mapStateToProps)(Header);
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>;
+      default:
+        return [
+          <li>Logout</li>
+        ];
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <ul className="right">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-function mapStateToProps (state) {
-    return {auth: state.auth}
-};
+function mapStateToProps({ auth }) {
+  return { auth };
+}
 
 export default connect(mapStateToProps)(Header);
