e74b9a7b53c387a3a4f44112981fa89b438da74c
cipdv <cip.devries@gmail.com>
2020-03-29 13:44:33 -0400

    up to lec 129

--- index.js
+++ index.js
@@ -6,20 +6,13 @@ const keys = require ('./config/keys');
 const bodyParser = require ('body-parser');
 require ('./models/User');
 require ('./services/passport');
+require('./models/Survey');
 
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
-
-const path = require ('path');
-app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 app.use(bodyParser.json());
 app.use (
@@ -41,6 +34,17 @@ app.use (
 
 require ('./routes/authRoutes')(app);
 require ('./routes/billingRoutes')(app);
+require ('./routes/surveyRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+const path = require ('path');
+app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
