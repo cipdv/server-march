27dda996bd21cbd20b947b443c919e2fa0dff31d
cipdv <cip.devries@gmail.com>
2020-04-16 08:47:25 -0400

    up to lec 155

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
 
 import Header from './Header';
-import { Component } from 'react';
 import Landing from './Landing';
-const Dashboard = ()=> <h2>dashboard</h2>;
-const SurveyNew = ()=> <h2>survey new</h2>;
-
-
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew'
 
 class App extends Component{
     componentDidMount() {
@@ -22,8 +19,8 @@ class App extends Component{
                 <BrowserRouter>
                     <div>
                         <Header />
-                        <Route path="/" exact component={Landing} />
-                        <Route path="/surveys" component={Dashboard} />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
                         <Route path="/surveys/new" component={SurveyNew}/>
                     </div>
                 </BrowserRouter>
